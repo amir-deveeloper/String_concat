f53c16dfbf16212c73ae477c06bbf4b249ba3512
amir <khamidullin.amir@gmail.com>
2022-02-18 15:15:29 +0300

    Hailstone problem

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,37 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(sequentialSearch(new int[]{1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}, 15));
+
+        int n = 17;
+        while (n != 1){
+
+            if (n % 2 == 0){
+
+                n = n / 2;
+
+            }
+            else{
+
+                n = 3*n + 1;
+
+            }
+
+        }
+
+
+
+
+        //System.out.println(sequentialSearch(new int[]{1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}, 15));
     }
 
+
+
+
+
+
+
+
+/*
     public static int sequentialSearch(int[] arr, int x) {
         boolean l = false;
         int n = 0;
@@ -37,6 +65,8 @@ public class Main {
 
 
     }
+
+ */
 
 
 
