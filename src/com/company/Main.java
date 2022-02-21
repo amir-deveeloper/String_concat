107b3c96c86b8da919739be7797e9a8c3a526f53
amir <khamidullin.amir@gmail.com>
2022-02-21 14:18:51 +0300

    String concat

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,30 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int n = 17;
-        while (n != 1){
+        int[] arr = {3, 6, 1, 23};
 
-            if (n % 2 == 0){
+        for (int i = 0; i < arr.length; i++){
 
-                n = n / 2;
+            if (i == 0){
+
+                System.out.print(arr[i] + " is first, ");
+            }
+
+            else if (i == arr.length-1){
+
+                System.out.println(arr[i] + " is last");
 
             }
+
             else{
 
-                n = 3*n + 1;
+                System.out.print(arr[i] + " is after " + arr[i-1] + " and below " + arr[i+1] +", ");
 
             }
 
@@ -23,97 +33,18 @@ public class Main {
 
 
 
-        //System.out.println(sequentialSearch(new int[]{1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}, 15));
-    }
-
-
-
-
-
-
-
-
-/*
-    public static int sequentialSearch(int[] arr, int x) {
-        boolean l = false;
-        int n = 0;
-
-        while (l == false) {
-
-            for (int i = 0; i < arr.length; i++) {
-
-                if (arr[i] == x) {
-                    l = true;
-                    n = 1;
-
-                    return i;
-                }
-
-            }
-
-            if (n == 0){
-
-                return -1;
-
-            }
-
-
         }
 
-            return -1;
+
+
+
+
 
 
 
     }
 
- */
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    public static int binarySearch(int [] arr, int x){
-
-        int l = 0;
-        int r = arr.length - 1;
-        while (l <= r){
-
-            int m = l + (r - l)/2;
-            if (arr[m] == x){
-
-                return m;
-            }
-            if (x > arr[m]){
-
-                l = m+1;
-
-            }else{
-                r = m -1;
-            }
-
-        }
-
-        return -1;
-    }
-
-     */
-
-
-
-
-
-}
